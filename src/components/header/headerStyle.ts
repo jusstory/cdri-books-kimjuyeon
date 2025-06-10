9890ffda63c3be95bfcc0b9e3a82dfe5e3be6e0f
jusstory <jjoo8829@naver.com>
2025-06-10 19:26:07 +0900

    [feat] header 개발

--- src/components/header/headerStyle.ts
+++ src/components/header/headerStyle.ts
@@ -1,3 +1,21 @@
 import styled from 'styled-components';
 
-export const HeaderStyle = styled.header``;
+export const HeaderStyle = styled.header`
+  display: flex;
+  align-items: center;
+  width: 80%;
+  height: 80px;
+  margin: 0 auto;
+`;
+
+export const NavStyle = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  gap: 40px;
+
+  button:hover {
+    color: red;
+  }
+`;
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,44 @@
 import React from 'react';
-import { HeaderStyle } from './headerStyle';
+import Image from 'next/image';
+import { HeaderStyle, NavStyle } from './headerStyle';
+import Button from '@components/common/button';
+import { useRouter } from 'next/router';
+import Text from '@components/common/text';
 
 function Header() {
-  return <HeaderStyle>Header</HeaderStyle>;
+  const router = useRouter();
+
+  const menuData = [
+    {
+      menu: '도서 검색',
+      url: '#',
+    },
+    {
+      menu: '내가 찜한 책',
+      url: '#',
+    },
+  ];
+
+  const handleClick = (url: string) => {
+    router.push(`/${url}`);
+  };
+
+  return (
+    <HeaderStyle>
+      <Image src="/images/logo.png" alt="logo" width={207} height={19} />
+      <NavStyle>
+        {menuData.map((item) => {
+          return (
+            <li key={item.menu}>
+              <Button type="text" onClick={() => handleClick(item.url)}>
+                <Text type="body1">{item.menu}</Text>
+              </Button>
+            </li>
+          );
+        })}
+      </NavStyle>
+    </HeaderStyle>
+  );
 }
 
 export default Header;
