e2bc34f1520456300b3ee5a49d0464eea9f48fd7
jusstory <jjoo8829@naver.com>
2025-06-10 11:12:56 +0900

    [chore] 프로젝트 초기 환경 설정 및 스타일 설정

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | My Next App</title>
+        <meta name="description" content="" />
+      </Head>
+      <main>
+        <h1>Welcome to My Next.js App!</h1>
+      </main>
+    </>
+  )
+}
